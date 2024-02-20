5cee76ea688ac34cd5f232a1f0f00787974c5b6c
Andrew Esterson <andrewesterson1@gmail.com>
2024-02-20 11:36:38 +0000

    switched to using ripgrep internals

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,67 @@
-use std::fs;
 use std::io;
 use std::error::Error;
 use std::process::Command;
-use walkdir::{DirEntry, WalkDir};
-use aho_corasick::AhoCorasick;
+use {
+    grep::{
+        regex::RegexMatcher,
+        searcher::{BinaryDetection, SearcherBuilder, sinks::UTF8},
+    },
+    walkdir::WalkDir,
+};
 use urlencoding::encode;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut matched_files = Vec::<String>::new();
-    let mut file_num: u8 = 0;
-    let walker = WalkDir::new(&config.file_path).into_iter();
-    let ac = AhoCorasick::new(patterns).unwrap();
-    for result in walker.filter_entry(|e| !is_hidden(e)) {
+    let mut matches: Vec<(String, u64, String)> = vec![];
+    let matcher = RegexMatcher::new_line_matcher(&config.query)?;
+    let mut searcher = SearcherBuilder::new()
+        .binary_detection(BinaryDetection::quit(b'\x00'))
+        .line_number(true)
+        .build();
+    for result in WalkDir::new(&config.file_path) {
         match result {
             Ok(entry) => {
-                let filename = entry.file_name().to_string_lossy();
-                let file = entry.path().to_string_lossy().into_owned();
-                match fs::read_to_string(entry.path()) {
-                    Ok(contents) => {
-                        for line in search(&config.query, &contents) {
-                            if !matched_files.contains(&file) {
-                                file_num += 1;
-                                matched_files.push(file.clone())
-                            };
-                            println!("({0}) {filename}: {line}", file_num.to_string());
-                        }
+                if !entry.file_type().is_file() {
+                    continue;
+                }
+                let result = searcher.search_path(
+                    &matcher,
+                    entry.path(),
+                    UTF8(|lnum, line| {
+                        matches.push((entry.path().to_string_lossy().into_owned(), lnum, line.to_string()));
+                        Ok(true)
                     }
-                    Err(_) => {
-                        continue;
-                    }
+                ));
+                if let Err(err) = result {
+                    eprintln!("{}: {}", entry.path().display(), err);
                 }
             }
-            Err(_) => {
-                continue;
-            }
+            Err(err) => {
+                    eprintln!("error in walking dirs: {}", err);
+                    continue;
+                }
         }
     }
-    if matched_files.len() > 0 {
+    matches.sort_by_key(|&(ref s, _, _)| s.clone());
+    let mut matched_files: Vec<&String> = vec![];
+    let mut count = 0;
+    for matched in &matches {
+        let filename = &matched.0;
+        println!("({}) {}: {}",
+            count,
+            matched.0,
+            matched.2);
+        if !matched_files.contains(&filename) {
+            matched_files.push(filename);
+            count += 1;
+        }
+    }
+    if matches.len() > 0 {
         let _response = user_response(matched_files);
     }
     Ok(())
 }
 
-fn user_response(v: Vec<String>) -> Result<(), Box<dyn Error>> {
+fn user_response(matched_files: Vec<&String>) -> Result<(), Box<dyn Error>> {
     loop {
         let mut requested_num = String::new();
         io::stdin()
@@ -55,14 +74,14 @@ fn user_response(v: Vec<String>) -> Result<(), Box<dyn Error>> {
                 continue;
             }
         };
-        if v.len() < requested_num || requested_num == 0 {
+        if matched_files.len() < requested_num || requested_num == 0 {
             println!("Invalid number");
             continue;
         }
         let _status = Command::new("open")
             .arg("-a")
             .arg("obsidian")
-            .arg(format!("obsidian://open?path={}", encode(&v[requested_num - 1])))
+            .arg(format!("obsidian://open?path={}", encode(&matched_files[requested_num])))
             .status();
         break Ok(());
     }
@@ -87,20 +106,3 @@ impl Config {
     }
 }
 
-fn search<'a>(query: &String, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query.to_lowercase()) {
-            results.push(line);
-        }
-    }
-
-    results
-}
-
-fn is_hidden(entry: &DirEntry) -> bool {
-    entry.file_name()
-         .to_str()
-         .map(|s| s.starts_with("."))
-         .unwrap_or(false)
-}
